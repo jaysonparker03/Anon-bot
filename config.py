fe2c72625b6621facd7cf0ae2b80cb6d9c56a343
jaysonparker03 <kololkolol92@gmail.com>
2026-07-25 06:27:19 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# Токен твоего бота, полученный у @BotFather
+BOT_TOKEN = "8636416275:AAG5Kdi4QLS6vVqCPytjsxzlLedhfSPTZFI"
+
+# Твой Telegram ID (узнать можно через @getmyid_bot)
+OWNER_ID = 7645096638
+
+# Ограничение на отправку сообщений (антиспам в секундах)
+COOLDOWN_SECONDS = 30
